349c3475e13b699d270c12cab3d625f647e822bd
Paul Mackay <paulmackay@uvic.ca>
2024-02-04 03:39:19 -0800

    everything working need to test on pico

--- t1/nonblocking_echo_server.py
+++ t1/nonblocking_echo_server.py
@@ -1,8 +1,6 @@
-'''
-GET /small.html HTTP/1.0\r\nConnection: keep-alive\r\n\r\n
-'''
 #!/usr/bin/env python3
 
+import sys
 import socket
 import select
 import queue
@@ -11,22 +9,24 @@ import os
 from datetime import datetime, timedelta
 from time import gmtime, strftime
 
+if len(sys.argv) != 3:
+    print(f"Usage: {sys.argv[0]} <ip_address> <port_number>")
+    sys.exit(1)
+else:
+    _, ip_address, port_number = sys.argv
+    port_number = int(port_number)  
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    server.setblocking(0)
+    server.bind((ip_address, port_number))
+    server.listen(5)
+
 response_400 = "HTTP/1.0 400 Bad Request\r\nConnection: close\r\n\r\n"
 code400 = "HTTP/1.0 400 Bad Request"
 code404 = "HTTP/1.0 404 Not Found"
 code200 = "HTTP/1.0 200 OK"
 
 # Create TCP/IP socket
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-server.setblocking(0)
-
-#“python3 sws.py ip_address port_number
-# Bind socket to address
-server.bind(("", 9000))
-
-# Start listening for incoming connections
-server.listen(5)
 
 inputs = [server]   # Sockets from which we expect to read
 outputs = []        # Sockets to which we expect to write
@@ -40,50 +40,48 @@ def handle_request(data, socket, ip, port):
     keep_alive = 0
     lnx = "\r\n"
     wds = "\r\n"
-    
-    re_det = r"^(.*?)\r\n(.*?)\r\n\r\n" 
-    det = re.match(re_det, data)
-    if  det:
-        current_request = data.split('\r\n')
-        request_line = current_request[0]
-        header = current_request[1]
-        con_type = header.split()[1]#.strip("\n")
-    else:
-        con_type = "close"
-        current_request = data.split('\r\n\r\n')
-        request_line = current_request[0]
-
-    #f_header = request_line.strip("\r\n")
-    method, f, version = request_line.split()
-    file = f.strip("/")
-
-    dir = os.path.dirname(os.path.abspath(__file__))
-    path = os.path.join(dir,file)
-    ## 400
 
     regex_400 = r"^(GET) (\/[^\s]+) (HTTP\/1\.0)" 
-    match = re.match(regex_400, request_line)
+    match = re.match(regex_400, data)
 
     if not match:
         code = code400
-        response = code400 + lnx + "Connection: close" +  "\r\n\r\n"
-    elif not os.path.isfile(path):
-        code = code404
-        response = code404 + lnx + "Connection: " + con_type + "\r\n\r\n"
-        if con_type == "keep-alive":
-            keep_alive = 1
+        response = code400 + lnx + "Connection: close" + "\r\n\r\n"
+        return response, code, keep_alive, ip, port
     else:
-        code = code200
-        with open(path, 'r') as file:
-            buffer = file.read()
 
-        file_content = buffer.strip("\r\n")
-        response = code200 + lnx + "Connection: " + con_type + "\r\n\r\n" + buffer
-        if con_type == "keep-alive":
-            keep_alive = 1
+        re_det = r"^(.*?)\r\n(.*?)\r\n\r\n" 
+        has_header = re.match(re_det, data)
+        if  has_header:
+            current_request = data.split('\r\n')
+            request_line = current_request[0]
+            header = current_request[1]
+            con_type = header.split()[1]
+        else:
+            con_type = "close"
+            current_request = data.split('\r\n\r\n')
+            request_line = current_request[0]
 
-    return response, code, keep_alive, ip, port
+        _, f, _ = request_line.split()
+        file = f.strip("/")
 
+        dir = os.path.dirname(os.path.abspath(__file__))
+        path = os.path.join(dir,file)
+
+        if not os.path.isfile(path):
+            code = code404
+            response = code404 + lnx + "Connection: " + con_type + "\r\n\r\n"
+            if con_type == "keep-alive":
+                keep_alive = 1
+        else:
+            code = code200
+            with open(path, 'r') as file:
+                buffer = file.read()
+            response = code200 + lnx + "Connection: " + con_type + "\r\n\r\n" + buffer
+            if con_type == "keep-alive":
+                keep_alive = 1
+
+        return response, code, keep_alive, ip, port
 
 # Close a socket
 def close_socket(s):
@@ -134,16 +132,6 @@ while inputs:
                     close_socket(s)
                     del last_activity[s]
 
-
-    '''
-    Format:
-    ■
-    time: client_ip:client_port request; response
-    ○
-    E.g.
-    ■
-    Wed Sep 15 21:44:35 PDT 2021: 192.168.1.100:54321 GET /sws.py HTTP/1.0; HTTP/1.0 200 OK
-    '''
     for s in writable:
         try:
             next_msg = message_queues[s].get_nowait()
@@ -157,7 +145,7 @@ while inputs:
             msg = next_msg
             f_msg = msg
             f_msg = f_msg.split("\r\n")[0]
-            response,code,conn_type, f_ip, f_port = handle_request(msg,s, f_ip, f_port)
+            response, code, conn_type, f_ip, f_port = handle_request(msg, s, f_ip, f_port)
             s.send(response.encode())
 
             print(f"{current_time}: {f_ip}:{f_port} {f_msg}; {code} ")
